8165ee31cd77b8b5ab8c063ed5bd52498a997dfe
Qianxi Li <qianxi@ualberta.ca>
2021-01-31 23:55:05 -0800

    second commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.urls import include, path
+
 
 urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
     url(r'^admin/', admin.site.urls),
 ]
